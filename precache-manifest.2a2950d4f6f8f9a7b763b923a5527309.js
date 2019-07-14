52c8f0d5b586005f455b845c96450f940c0e85d4
veeteefunk <victormarinviloria@gmail.com>
2019-07-14 13:10:39 +0200

    Updates

---
+++ precache-manifest.2a2950d4f6f8f9a7b763b923a5527309.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebcfd78c86a88c6e79e56e49a8403992",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "c9be3e021899368a7fff",
+    "url": "/ToDoList/static/css/main.024f5c4f.chunk.css"
+  },
+  {
+    "revision": "5a59ff517d3b0db0a0f9",
+    "url": "/ToDoList/static/js/2.04204285.chunk.js"
+  },
+  {
+    "revision": "c9be3e021899368a7fff",
+    "url": "/ToDoList/static/js/main.8580d927.chunk.js"
+  },
+  {
+    "revision": "6a71c43a298729cf07dd",
+    "url": "/ToDoList/static/js/runtime~main.67fadf76.js"
+  }
+]);
